bd066acfb62ad70db6d5db74dfd324b91ad76ead
Freesia <freesiaxstar@gmail.com>
2020-04-24 17:10:33 +0800

    建立静态站点
    online

---
+++ precache-manifest.c0fc3ea35087d70a7925fee5192dde9b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960cd99352532aa4cfb71deb49396563",
+    "url": "0a1928e05c69dd869473.worker.js"
+  },
+  {
+    "revision": "eefb8f620c93159e46b4",
+    "url": "css/app.2a97a82f.css"
+  },
+  {
+    "revision": "bae1e36f2866fb672225",
+    "url": "css/chunk-vendors.8cf7dd44.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "0a9e9c77195503ca56221eef7cc1641f",
+    "url": "index.html"
+  },
+  {
+    "revision": "eefb8f620c93159e46b4",
+    "url": "js/app.108bc2cf.js"
+  },
+  {
+    "revision": "bae1e36f2866fb672225",
+    "url": "js/chunk-vendors.6687413a.js"
+  },
+  {
+    "revision": "28f3e13ec88073aa1b85b7d66358f613",
+    "url": "manifest.json"
+  }
+]);
